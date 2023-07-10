a205d693ec3aff4c14dc906da28975bac8cddcee
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-07-10 21:45:46 +0530

    Create Use of var let const.js

---
+++ Pract 1/Use of var let const.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<script>
+var x = "Hello";
+console.log("Value of x:", x);
+let y = 42;
+console.log("Value of y:", y);
+const z = true;
+console.log("Value of z:", z);
+</script>
+</body>
+</html>
