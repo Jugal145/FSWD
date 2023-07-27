fcdfdaf061c3eb3435f08c89050ec6a143ec615b
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-07-27 15:44:08 +0530

    Create os module.js

---
+++ Pract 3/os module.js
@@ -0,0 +1,48 @@
+const os = require("os");
+
+
+const username = os.userInfo().username;
+console.log("Username:", username);
+
+
+const homeDir = os.homedir();
+console.log("Home Directory:", homeDir);
+
+
+const platform = os.platform();
+console.log("Operating System Platform:", platform);
+
+
+//Function
+const os = require('os');
+
+
+function displayMemoryInfo() {
+  const totalMemory = os.totalmem();
+  console.log('Total Memory:', formatBytes(totalMemory));
+const freeMemory = os.freemem();
+  console.log('Free Memory:', formatBytes(freeMemory));
+
+
+  const percentFreeMemory = ((freeMemory / totalMemory) * 100).toFixed(2);
+  console.log('Percentage of Free Memory:', percentFreeMemory + '%');
+}
+
+
+function formatBytes(bytes) {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let size = bytes;
+  let unitIndex = 0;
+
+
+  while (size >= 1024 && unitIndex < units.length - 1) {
+    size /= 1024;
+    unitIndex++;
+  }
+
+
+  return size.toFixed(2) + ' ' + units[unitIndex];
+}
+
+
+displayMemoryInfo();
