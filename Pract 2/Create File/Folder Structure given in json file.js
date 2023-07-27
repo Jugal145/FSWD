eb280c6776c1f9b8811df6c7ff9992b45ca553aa
Jugal145 <97076868+Jugal145@users.noreply.github.com>
2023-07-27 15:40:23 +0530

    Create Folder Structure given in json file.js

---
+++ Pract 2/Create File/Folder Structure given in json file.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+
+
+// Function to create file/folder structure recursively
+function createStructure(basePath, structure) {
+  for (const item of structure) {
+    const itemPath = path.join(basePath, item.name);
+
+
+    if (item.type === 'file') {
+      fs.writeFileSync(itemPath, item.content || '');
+      console.log(`File created: ${itemPath}`);
+    } else if (item.type === 'folder') {
+      fs.mkdirSync(itemPath);
+      console.log(`Folder created: ${itemPath}`);
+      createStructure(itemPath, item.children || []);
+    }
+  }
+}
+
+
+// Read the JSON file
+const jsonFilePath = 'structure.json';
+const structure = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+
+
+// Specify the base path where the structure will be created
+const basePath = './destination';
+
+
+// Create the file/folder structure
+createStructure(basePath, structure);
+
+
+
+JASON DATA
+[
+    {
+      "name": "folder1",
+      "type": "folder",
+      "children": [
+        {
+          "name": "file1.txt",
+          "type": "file",
+          "content": "Content of file 1"
+        },
+        {
+          "name": "folder2",
+          "type": "folder",
+          "children": [
+            {
+              "name": "file2.txt",
+              "type": "file",
+              "content": "Content of file 2"
+            }
+          ]
+        }
+      ]
+    }
+  ]
